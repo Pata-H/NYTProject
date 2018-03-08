45e550d88e804da5998bbc15c12c66404d044c95
cfdahill <cfdahill@gmail.com>
2018-03-07 19:01:12 -0700

    final attempt

--- nytimesBE.js
+++ nytimesBE.js
@@ -4,7 +4,7 @@ var endYear = "&end_date=";
 var endMonth = "1231"
 var searchTerm = "&q=";
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=285f60c591b149798efb85d7cb467bdb";
-
+//templateliterals <-- look into this
 //var queryURL = url+searchTerm+beginYear+beginMonth+endYear+endMonth;
 console.log(queryURL);
 
@@ -13,16 +13,19 @@ $('.getQuery').click(function(){
     startYear += $('.startYear').val();
     endYear += $('.endYear').val();
     var queryURL = url+searchTerm+beginYear+beginMonth+endYear+endMonth;
-
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET", 
     }).done(function(response){
+        for(i = 0; i <docs.length; i++) {
         var story = $("<span>")
-        var title = response.docs.headline.main;
+        var title = response.docs[i].headline.main;
         story.attr("class", "story");
-        story.append("<h1><a> " + title + " src='" + response.docs.web_url + "' </a></h1>");
-        story.append(response.docs.abstract);
+        story.append("<h1><a src='" + response.docs[i].web_url + "'>" + title + "' </a></h1>");
+        story.append(response.docs[i].abstract);
         $("#storySection").prepend(story);
+        console.log(response);
+        }
     })
 })
